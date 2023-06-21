a8257979cb7b5feace88a752bb06725cf2ba6a18
Eduardo Rivas Zapata <zapata@redhat.com>
2023-06-21 13:58:07 -0600

    update values pipelines

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ oc apply -f gitops-deployment/gitops-subscription.yaml
 #Obtener password de usuario admin de argocd
 oc get secret openshift-gitops-cluster -n openshift-gitops -o jsonpath="{.data['admin\.password']}" | base64 -d
 
+#crear aplicacion en ArgoCD
+oc create -f gitops-deployment/operators-app-argocd.yaml -n openshift-gitops
